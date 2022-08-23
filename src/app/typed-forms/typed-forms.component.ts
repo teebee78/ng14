21380282f88f99440bf0a61cb3d625ca012cb13f
Tee Bee <teebee78@gmail.com>
2022-08-23 21:21:52 +0200

    typed form example

--- src/app/typed-forms/typed-forms.component.ts
+++ src/app/typed-forms/typed-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-typed-forms',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypedFormsComponent implements OnInit {
 
-  constructor() { }
+  form = this.fb.group({
+    email: ['', [ Validators.required, Validators.email]], 
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+  formValue?: FormValue;
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    this.formValue = this.form.value;
+  }
+}
+interface FormValue {
+  email?: string | null; 
+  password?: string | null;
 }
